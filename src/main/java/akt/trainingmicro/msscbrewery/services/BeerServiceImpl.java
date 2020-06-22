e6cc4f511bafc3a8e16b65304c65c8b60b3a9ad2
arodrigues_freebalance <arodrigues@freebalance.com>
2020-06-22 16:08:27 +0100

    Fixed TODO Annotation.

--- src/main/java/akt/trainingmicro/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/akt/trainingmicro/msscbrewery/services/BeerServiceImpl.java
@@ -27,7 +27,7 @@ public class BeerServiceImpl implements BeerService {
 
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
-        //todo impl - would add a real impl to update beer
+        // TODO  impl - would add a real impl to update beer
     }
 
     @Override
